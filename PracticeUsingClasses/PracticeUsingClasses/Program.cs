e59270af7c35555578b0cc1cab7263060474f0ab
Carrie Ryan <cryan1205@gmail.com>
2017-03-07 21:45:01 -0500

    initial commit

---
+++ PracticeUsingClasses/PracticeUsingClasses/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeUsingClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name: ");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your email: ");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Please enter your phone number: ");
+            string phonenumber = Console.ReadLine();
+
+
+            Student student = new Student(firstName, lastName, email, phonenumber);
+
+            student.PrintInfo();
+
+        }
+    }
+}
---
+++ PracticeUsingClasses/PracticeUsingClasses/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeUsingClasses
+{
+    class Student
+    {
+        string firstName;
+        string lastName;
+        string fullName;
+        string email;
+        string phoneNumber;
+
+
+
+        public string FullName
+        {
+            get { return this.firstName + " " + this.lastName; }
+
+        }
+
+        public string Email
+        {
+            get { return this.email; }
+            set { this.email = value; }
+
+        }
+
+        public string PhoneNumber
+        {
+            get { return this.phoneNumber; }
+            set { this.phoneNumber = value; }
+        }
+
+        public Student()
+        {
+            //this is default constructor
+        }
+
+        public Student(string firstName, string lastName, string email, string phoneNumber)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.phoneNumber = phoneNumber;
+        }
+
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Hello, {0}!", this.FullName);
+            Console.WriteLine("Your email is {0}", this.email);
+            Console.WriteLine("Your phone number is {0}", this.phoneNumber);
+        }
+    }
+}
